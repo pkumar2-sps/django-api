a2bdaf0928df0f45ad0da1772350531e0eb52d9c
Prakash Kumar <39080992+pkumar2-sps@users.noreply.github.com>
2018-07-09 12:10:01 +0530

    Update test file

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,5 +1,5 @@
 import requests
 
 def test_demo():
-    response = requests.get('http://127.0.0.1:8000/' + "api/v1/reports/1")
+    response = requests.get('http://127.0.0.1:8000')
     assert (response.status_code == 200)
