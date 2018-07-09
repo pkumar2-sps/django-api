0552a73bc2b51b92ae7e04df0695ca54dabcecaf
Prakash Kumar <39080992+pkumar2-sps@users.noreply.github.com>
2018-07-09 13:12:10 +0530

    response = requests.get('http://localhost:8000/')

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,6 +1,5 @@
 import requests
 
 def test_demo():
-    response1 = requests.get('http://127.0.0.1:8000/')
-    response2 = requests.get('http://localhost:8000/')
-    assert (response1 == response2)
+    response = requests.get('http://localhost:8000/')
+    assert (response.status_code == 200)
