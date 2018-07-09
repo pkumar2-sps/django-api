9f9ef9798fe2093190ddedbbe30a0f05e2db8dab
Prakash Kumar <39080992+pkumar2-sps@users.noreply.github.com>
2018-07-09 11:47:30 +0530

    web_settings
    web_settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import django
 from django.conf import settings
 
-from label_service_api import settings as label_service_api_settings
+from web import settings as web_settings
 
 def pytest_configure():
-	settings.configure(**vars(label_service_api_settings))
+	settings.configure(**vars(web_settings))
 	settings.ALLOWED_HOSTS+=['testserver']
 	django.setup()
