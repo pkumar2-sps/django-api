cce67cfb00670d53fab6052f4d8b33882734671e
Prakash Kumar <39080992+pkumar2-sps@users.noreply.github.com>
2018-07-09 13:15:03 +0530

    localhost > 0.0.0.0

--- tests/test_demo.py
+++ tests/test_demo.py
@@ -1,5 +1,5 @@
 import requests
 
 def test_demo():
-    response = requests.get('http://localhost:8000/')
+    response = requests.get('http://0.0.0.0:8000/')
     assert (response.status_code == 200)
